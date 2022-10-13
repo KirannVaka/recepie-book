7dd0c6901630fd2c8add086ace4002824cf41d2f
Kiran Vaka <saikirann.vaka@gmail.com>
2022-10-13 14:33:31 +0800

    imported and setup 'axios' to fetch data

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,9 +1,9 @@
 import { useGolbalContext } from "../context";
 
 const Meals = () => {
-  const context = useGolbalContext();
-  console.log(context);
-  return <h1>Meals Component</h1>;
+  const { meals } = useGolbalContext();
+  console.log(meals.map((meal) => meal.idMeal));
+  return <h1>{meals.map((meal) => " " + meal.idMeal)}</h1>;
 };
 
 export default Meals;
--- src/context.jsx
+++ src/context.jsx
@@ -1,17 +1,32 @@
-import React, { useContext } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 
 const AppContext = React.createContext();
+
+const allMealsUrl = "https://www.themealdb.com/api/json/v1/1/search.php?s=a";
+const randomMealUrl = "https://www.themealdb.com/api/json/v1/1/random.php";
 
 const useGolbalContext = () => {
   return useContext(AppContext);
 };
 
 const AppProvider = ({ children }) => {
-  return (
-    <AppContext.Provider value={{ name: "John", role: "Student" }}>
-      {children}
-    </AppContext.Provider>
-  );
+  const [meals, setMeals] = useState({});
+
+  const fetchMeals = async (url) => {
+    try {
+      const { data } = await axios.get(url);
+      setMeals(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMeals(allMealsUrl);
+  }, []);
+
+  return <AppContext.Provider value={meals}>{children}</AppContext.Provider>;
 };
 
 export { AppContext, AppProvider, useGolbalContext };
